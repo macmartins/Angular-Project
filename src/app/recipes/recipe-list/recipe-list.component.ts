3840b937ed2d627ba2c8565cf79bd42b3794bf79
macmartins <firewolf949@gmail.com>
2023-01-27 23:03:40 +0000

    Setup header + recipes sections

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ];
 }
